6a7be4a7f8fb2a47f2468897bff229da2ad4cadc
Kartik <kartikgupta1805@gmail.com>
2022-02-20 19:08:38 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Tictactoe from './Tictactoe'
+
+export class App extends Component {
+  render() {
+    return (
+      <>
+      <Tictactoe />
+      </>
+    )
+  }
+}
+
+export default App
+
